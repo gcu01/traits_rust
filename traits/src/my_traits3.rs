53bc9ee4cd920432dbe730f8f7f3a657d3bcd96b
Gabriel <gabrielcampeanu@gmail.com>
2025-01-24 12:52:57 +0100

    traits with generic and lifetime parameters

---
+++ traits/src/my_traits3.rs
@@ -0,0 +1,36 @@
+pub trait Traits3<'a, T> {
+    fn func1(self, arg: T) -> Self;
+    fn func2(self, arg: &'a Self) -> Self;
+    fn func3(self, arg: &'a T) -> Self;
+}
+
+struct Str4 {x: i32}
+
+impl<'a> Traits3<'a, i32> for Str4 {
+    fn func1(self, arg: i32)-> Str4 {
+        Str4{x: arg}
+    }
+    fn func2(self, arg: &'a Self) -> Str4{
+        Str4{x: arg.x}
+    }
+    fn func3(self, arg: &'a i32)-> Str4 {
+        Str4{x: *arg}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn traits3_test1() {
+        let mut s: Str4 = Str4{x:1};
+        let s2: Str4 = Str4{x:2};
+        s = Str4::func1(s, 4);
+        assert_eq!(4, s.x);
+        s = s.func2(&s2);
+        assert_eq!(2, s.x);
+        s = s.func3(&3);
+        assert_eq!(3, s.x);
+    }
+}
